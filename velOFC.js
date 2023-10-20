60f17df3c4846b58fc3795dfd95690ad088c0aa7
the.carneiro <betocarneiro87@gmail.com>
2023-10-19 23:15:09 -0300

    v3
    index
        campo para exibição de lat e long desativado
    
    vel.css
        visual para pokebola
    
    velOFC
        removida função de registrar lat e long.
            recuperar este da V1
        verificar exibição da velMax em valor inteiro
            decidir o metodo de arredondamento

--- velOFC.js
+++ velOFC.js
@@ -3,45 +3,44 @@ const velocidade = document.querySelector("#velocidade")
 
 let lat = document.querySelector("#lat");
 let lon = document.querySelector("#lon");
-
-let topSpeed = document.querySelector("#topSpeed"); 
-let varSpeed = null;
+let topSpeed = document.querySelector("#topSpeed");
 
 let watchId = null;
 const option= {enableHighAccuracy:true}
 
 velocimetro.addEventListener("click",()=>{
+    //cria o click no mostrador pra dar start/stop
     if (!watchId){
         watchId = navigator.geolocation.watchPosition(updatePosition,handleError,option);
     }else{
         navigator.geolocation.clearWatch(watchId);
         watchId=null;
         velocidade.textContent="--";
-        topSpeed.textContent="--"
+        topSpeed.textContent="--";
     }
 })
 
 function handleError(error){
-    console.log(error.message);
+    console.log(error.message);//caso dê erro, exibe no console 
 }
 
 function updatePosition(position){
-    console.log(position.coords)
-    if(position.coords.speed<0){
+    console.log(position.coords);//exibe as coordenadas no console
+    if(position.coords.speed<0){//se a velocidade atual for<0 faz nada
         return;
     }else{
+        //atualiza a velocidade atual
         velocidade.textContent=(position.coords.speed*3.6).toFixed(1);
-        lat.textContent=position.coords.latitude;
-        lon.textContent=position.coords.longitude;
         
-        if (topSpeed.textContent<(position.coords.speed*3.6).toFixed(1)){
-            topSpeed.textContent=(position.coords.speed*3.6).toFixed(1)
-        }
-        
-        if(parseFloat(velocidade.textContent)>parseFloat(topSpeed)){
-            topSpeed.textContent=velocidade.textContent;
-        }else{
-            return;
-        }
+        //atualiza a velocidade máxima
+        //if((position.coords.speed*3.6).toFixed(1) > topSpeed.textContent){
+        //    topSpeed.textContent=(position.coords.speed*3.6).toFixed(1);
+        //}else{return;}
     }
 }
+
+// funcionando start/stop
+// funcionando velAtual
+// funcionando velMáxima, só tirar do comentário
+
+// verificar ajuste da velMáxima para o formato 00
